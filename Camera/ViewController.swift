1d6b3164045753a5e091e6119c1a0be43b9ac518
Jade Park <jpark4188@gmail.com>
2022-06-28 23:46:51 -0500

    Initial Commit

---
+++ Camera/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by Jade Park on 6/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+    }
+
+    @IBAction func choosePhotoTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func takeASelfieTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var imageDisplay: UIImageView!
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // update our photo with the selected photo
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageDisplay.image = selectedImage
+        }
+        // go back to our ViewController so the user can SEE the update
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
